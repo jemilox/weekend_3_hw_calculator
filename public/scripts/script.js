b4a31b7bb62e89760ff905405ddd0b9b7984cbcb
jemilox <jemilox@gmail.com>
2016-09-11 13:13:32 -0500

    final copy

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,50 @@
-console.log('sourced');
+//console.log('sourced');
+var calculationIn = '';
 
 $(document).ready(function(){
-  console.log('doc ready');
+  //console.log('doc ready');
+
+//get calculator button input
+  $('button').on('click', function () {
+    if ($(this).attr('data') === "="){
+      returnResults();
+    }else if ($(this).attr('data') === 'CE') {
+      calculationIn = '';
+      $('.outputScreen').html(calculationIn);
+    }else{
+    //console.log('in button click');
+    var nextInput = $(this).attr('data');
+
+    //put in calculationIn array
+    calculationIn += nextInput;
+    console.log(calculationIn);
+    //display current calculation
+    $('.outputScreen').html(calculationIn);
+    }
+  });
+
+  var returnResults = function () {
+    console.log('in returnResults');
+    var calculationToSend = {takeThis: calculationIn};
+    console.log(calculationToSend);
+    $.ajax({
+      type: 'POST',
+      url: '/calculate',
+      data: calculationToSend,
+      success: function (data) {
+        console.log(data);
+        var finalResults = data.takeThis;
+        console.log(finalResults);
+        $('.outputScreen').html(finalResults);
+      },
+      error: function (data) {
+        console.log('meow');
+        $('.outputScreen').html('Cannot compute!!');
+
+      }
+    });
+  };
+
 
 
 });
--- server/app.js
+++ server/app.js
@@ -4,6 +4,28 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded( {extended: false});
 
-app.listen('2001', 'localhost', function () {
-  console.log("I'm listening, Dave");
+app.listen('3000', 'localhost', function () {
+  console.log("port 3000 is listening");
 });//end server up
+
+app.get('/', urlencodedParser, function (req, res) {
+  console.log('base url hit');
+  res.sendFile(path.resolve('public/index.html'));
+  // res.send('meow');
+});//end get
+
+//anyone can use the public folder now
+app.use(express.static('public'));
+
+//get requested calculation and calculate
+app.post( '/calculate', urlencodedParser, function ( req, res) {
+  //console.log('in post');
+  //console.log('this is req', req.body);
+  //calculate string
+  var finishedCalculation = eval(req.body.takeThis);
+  console.log('finishedCalculation', finishedCalculation);
+  //send back
+  var sendBack = {takeThis: finishedCalculation};
+  res.send(sendBack);
+  console.log(sendBack);
+});
